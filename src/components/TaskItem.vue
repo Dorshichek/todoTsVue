e1ccb345b7d66867208e139c49936db3e464a1bf
Dmitrii <pavluchenko96.org@mail.ru>
2022-06-13 20:43:22 +0300

    add checkbox

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="task-item">
-    <input class="task-item__checkbox" type="checkbox">
+    <input class="task-item__checkbox" type="checkbox" v-model="isChecked">
     <div class="task-item__text">
       <span class="task-item__title">{{ task.title }}</span>
       <p class="task-item__description">{{ task.text }}</p>
@@ -21,11 +21,21 @@ import MyButton from "@/components/Ui/MyButton";
 export default defineComponent({
   name: "TaskItem",
   components: {MyButton},
+  data() {
+    return {
+      isChecked: false,
+    }
+  },
   props: {
     task: {
       type: Object,
     }
   },
+  methods: {
+    doneTask() {
+      const isChecked = this.isChecked
+    }
+  }
 })
 </script>
 
